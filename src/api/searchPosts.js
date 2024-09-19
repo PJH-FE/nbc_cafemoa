07a2f6fcc4ef2fc45773d8d34a40c35416e54716
stopbrother <stopbro0323@naver.com>
2024-09-19 20:11:55 +0900

    delete

--- src/api/searchPosts.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/searchPosts';
-
-export const getFilteredData = async keyword => {
-  const { data } = await axios.get(`${API_URL}/search?keyword=${keyword}`);
-  return data;
-};
