f7dc83d837f6a9ed5dc8d094d9cd767a0bc29c71
stopbrother <stopbro0323@naver.com>
2024-09-19 21:54:06 +0900

    delete: searchStore

--- src/zustand/searchStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useSearchStore = create(set => ({
-  searchKeyword: '',
-  setSearchKeyword: state => set({ searchKeyword: state }),
-}));
-
-export default useSearchStore;
