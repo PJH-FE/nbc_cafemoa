353f686a9b0f641e4a04812c4b5124d427fd3d44
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-09-19 20:00:45 +0900

    fix:유저의 북마크한 글 불러오기 코드 작성

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -5,12 +5,8 @@ import axios from 'axios';
 
 const Bookmark = () => {
   const getBookmarks = async () => {
-    const result = await axios.get(`http://localhost:4000/users?user_id=asy13_embed=articles`);
-  };
-
-  const fetchBookmarkedArticles = async () => {
-    const res = await DATA_API.get('/articles?isBookmarked=true');
-    return res.data;
+    const result = await DATA_API.get(`/users?user_id=asy13&_embed=articles`);
+    return result.data;
   };
 
   const {
@@ -19,17 +15,26 @@ const Bookmark = () => {
     isError,
   } = useQuery({
     queryKey: ['bookmarkedArticles'],
-    queryFn: fetchBookmarkedArticles,
+    queryFn: getBookmarks,
   });
 
   if (isPending) return <div>로딩중입니다...</div>;
   if (isError) return <div>에러가 발생했습니다...</div>;
 
+  const bookmarked = bookmarks[0].bookmarked;
+  const arr = bookmarks[0].articles.filter(article => {
+    console.log('article.id', article.id);
+    if (bookmarked.some(id => id === article.id)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
   return (
     <div className="px-10">
       <h1 className="mt-2 mb-3 text-xl font-bold">북마크한 게시물</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-7">
-        {bookmarks.map(bookmark => (
+        {arr.map(bookmark => (
           <Link to={`/detail?article_id=${bookmark.id}`} key={bookmark.id}>
             <div key={bookmark.id} className="overflow-hidden border rounded-lg shadow-lg w-72">
               <img src={bookmark.thumbnail} alt={bookmark.title} className="object-cover w-full h-64" />
