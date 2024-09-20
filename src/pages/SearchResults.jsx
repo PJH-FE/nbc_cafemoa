d06100d8a0824aa900c8b76d7d31a6fda95b5dd1
stopbrother <stopbro0323@naver.com>
2024-09-20 14:20:50 +0900

    delete: 콘솔 및 안쓰는 import 삭제

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,7 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 import { useSearchedArticles } from '../queries/searchArticleQuery';
-import { useState } from 'react';
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
@@ -11,7 +9,7 @@ const SearchResults = () => {
 
   if (isPending) return <p>로딩중</p>;
   if (error) return <p>에러: {error.message}</p>;
-  console.log('articles', articles);
+
   return (
     <div>
       <p>{articles.length}개의 게시물이 있습니다.</p>
