fbfb4165e715115ee04c9da8a85385dc7c2b76eb
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-09-22 01:28:51 +0900

    fix: cafeinfo페이지 불러오기

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -53,22 +53,39 @@ const Bookmark = () => {
     <div className="px-10">
       <h1 className="mt-2 mb-3 text-xl font-bold">북마크한 게시물</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-7">
-        {bookmarkedArticles.map(bookmark => (
-          <Link to={`/detail?article_id=${bookmark.id}`} key={bookmark.id}>
-            <div key={bookmark.id} className="overflow-hidden border rounded-lg shadow-lg w-72">
-              <img
-                src={bookmark.thumbnail ?? defaultUrl}
-                alt={bookmark.title}
-                className="object-cover w-full h-64"
-              />
-              <div className="p-4">
-                <p className="text-sm text-gray-500">{bookmark.region}</p>
-                <h3 className="text-lg font-semibold">{bookmark.title}</h3>
-                <p className="text-sm text-blue-500">#{bookmark.category}</p>
+        {bookmarkedArticles.map(bookmark =>
+          bookmark.author_id ? (
+            <Link to={`/detail?article_id=${bookmark.id}`} key={bookmark.id}>
+              <div key={bookmark.id} className="overflow-hidden border rounded-lg shadow-lg w-72">
+                <img
+                  src={bookmark.thumbnail ?? defaultUrl}
+                  alt={bookmark.title}
+                  className="object-cover w-full h-64"
+                />
+                <div className="p-4">
+                  <p className="text-sm text-gray-500">{bookmark.region}</p>
+                  <h3 className="text-lg font-semibold">{bookmark.title}</h3>
+                  <p className="text-sm text-blue-500">#{bookmark.category}</p>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ) : (
+            <Link to={`/cafeinfo?id=${bookmark.id}`} key={bookmark.id}>
+              <div key={bookmark.id} className="overflow-hidden border rounded-lg shadow-lg w-72">
+                <img
+                  src={bookmark.thumbnail ?? defaultUrl}
+                  alt={bookmark.title}
+                  className="object-cover w-full h-64"
+                />
+                <div className="p-4">
+                  <p className="text-sm text-gray-500">{bookmark.region}</p>
+                  <h3 className="text-lg font-semibold">{bookmark.title}</h3>
+                  <p className="text-sm text-blue-500">#{bookmark.category}</p>
+                </div>
+              </div>
+            </Link>
+          ),
+        )}
       </div>
     </div>
   );
