171371165bd3b6cdbbb6d603f5cb43f49527acf3
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-09-21 18:09:07 +0900

    update: cafedb API 호출 합침

--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -11,36 +11,48 @@ const Bookmark = () => {
   // console.log('bookmarked', bookmarked);
 
   const apiHost = async () => {
-    const result = await DATA_API.get(`/articles`);
-    setArticles(result.data);
-    return result.data;
+    // articles, cafedb 내용 둘 다 호출
+    const articleResult = await DATA_API.get(`/articles`);
+    const adminResult = await DATA_API.get(`/cafedb`);
+    // console.log(articleResult);
+    // console.log(adminResult);
+
+    // 두 호출 합침
+    const result = [...articleResult.data, ...adminResult.data];
+    // console.log('result', result);
+
+    const arr = result.filter(article => {
+      console.log('article', article);
+      console.log('bookmarked', bookmarked);
+      if (bookmarked.some(id => id === article.id)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    setArticles(arr);
+    return arr;
   };
 
   const {
-    data: allArticles,
+    data: bookmarkedArticles,
     isPending,
     isError,
   } = useQuery({
     queryKey: ['articles'],
     queryFn: apiHost,
   });
+  console.log('bookmarkedArticles', bookmarkedArticles);
 
   if (isPending) return <div>로딩중입니다...</div>;
   if (isError) return <div>에러가 발생했습니다...</div>;
-
-  const arr = allArticles.filter(article => {
-    if (bookmarked.some(id => id === article.id)) {
-      return true;
-    } else {
-      return false;
-    }
-  });
 
   return (
     <div className="px-10">
       <h1 className="mt-2 mb-3 text-xl font-bold">북마크한 게시물</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-7">
-        {arr.map(bookmark => (
+        {bookmarkedArticles.map(bookmark => (
           <Link to={`/detail?article_id=${bookmark.id}`} key={bookmark.id}>
             <div key={bookmark.id} className="overflow-hidden border rounded-lg shadow-lg w-72">
               <img src={bookmark.thumbnail} alt={bookmark.title} className="object-cover w-full h-64" />
