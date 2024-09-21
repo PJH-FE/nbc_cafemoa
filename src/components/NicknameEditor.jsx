aa1c7d65ccd5380c603b40dbf9900b1ef683096e
derek0k <derek0kim@gmail.com>
2024-09-22 01:32:38 +0900

    💄 style: NicknameEditor 기본 스타일 구현

--- src/components/NicknameEditor.jsx
+++ src/components/NicknameEditor.jsx
@@ -2,29 +2,50 @@ import { useState } from 'react';
 
 export default function NicknameEditor({ changeNickname, userNickname }) {
   const [nickname, setNickname] = useState('');
+  const [isUpdateNickname, setIsUpdateNickName] = useState(false);
 
   const handleNicknameChange = e => {
     setNickname(e.target.value);
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleClick = e => {
     changeNickname(nickname);
     alert('변경이 완료되었습니다');
+    setIsUpdateNickName(prev => !prev);
     setNickname('');
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="flex">
-        <div>
-          <label>닉네임</label>
-          <input onChange={handleNicknameChange} value={nickname} placeholder={userNickname || ''} />
-        </div>
-        <button type="submit" className="border-green-900 border">
-          수정
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="flex flex-col">
+        {isUpdateNickname ? (
+          <div>
+            <input
+              onChange={handleNicknameChange}
+              value={nickname}
+              placeholder={userNickname}
+              className="focus:outline-[#3B3030] p-1 mr-2"
+            />
+            <button
+              className="text-white border bg-customHardBorder rounded-md px-3 py-1 mt-2 hover:bg-[#3B3030] transition"
+              onClick={handleClick}
+            >
+              수정
+            </button>
+          </div>
+        ) : (
+          <div>
+            <span className="text-xl font-bold mr-1">{userNickname}</span>
+            <span>님</span>
+            <button
+              className="ml-2 mt-2 bg-customHardBorder text-white bg-custom rounded-md px-3 py-1 hover:bg-[#3B3030] transition"
+              onClick={() => setIsUpdateNickName(prev => !prev)}
+            >
+              수정
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
